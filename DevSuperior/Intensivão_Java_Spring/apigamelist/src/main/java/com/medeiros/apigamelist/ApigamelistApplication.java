6286557a985cb487987c261dbb9cbf7dc6dcc4f0
Bruno <brunodemedeiros14@gmail.com>
2023-05-08 22:50:45 -0300

    Projeto intensivão Java Spring  criado.

---
+++ DevSuperior/Intensivão_Java_Spring/apigamelist/src/main/java/com/medeiros/apigamelist/ApigamelistApplication.java
@@ -0,0 +1,12 @@
+package com.medeiros.apigamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigamelistApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApigamelistApplication.class, args);
+  }
+}
---
+++ DevSuperior/Intensivão_Java_Spring/apigamelist/src/test/java/com/medeiros/apigamelist/ApigamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeiros.apigamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
