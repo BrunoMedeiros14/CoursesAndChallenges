b9fe232dd89a427ef578f4271b7ae72cbc22aac5
Bruno <brunodemedeiros14@gmail.com>
2023-05-11 22:43:28 -0300

    Reorder game in list feature.

--- DevSuperior/Intensivão_Java_Spring/apigamelist/src/main/java/com/medeiros/apigamelist/controllers/GameListController.java
+++ DevSuperior/Intensivão_Java_Spring/apigamelist/src/main/java/com/medeiros/apigamelist/controllers/GameListController.java
@@ -2,12 +2,15 @@ package com.medeiros.apigamelist.controllers;
 
 import com.medeiros.apigamelist.dto.GameListDTO;
 import com.medeiros.apigamelist.dto.GameMinDTO;
+import com.medeiros.apigamelist.dto.ReplacementDTO;
 import com.medeiros.apigamelist.services.GameListService;
 import com.medeiros.apigamelist.services.GameService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +34,17 @@ public class GameListController {
   public List<GameMinDTO> findByList(@PathVariable Long listId) {
     List<GameMinDTO> result = gameService.findByList(listId);
     return result;
+  }
+
+  @PostMapping(value = "/{listId}/replacement")
+  public void moveGameInList(
+    @PathVariable Long listId,
+    @RequestBody ReplacementDTO replacement
+  ) {
+    gameListService.moveGameInList(
+      listId,
+      replacement.getSourceIndex(),
+      replacement.getDestinationIndex()
+    );
   }
 }
--- DevSuperior/Intensivão_Java_Spring/apigamelist/src/main/java/com/medeiros/apigamelist/services/GameListService.java
+++ DevSuperior/Intensivão_Java_Spring/apigamelist/src/main/java/com/medeiros/apigamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.medeiros.apigamelist.services;
 
 import com.medeiros.apigamelist.dto.GameListDTO;
 import com.medeiros.apigamelist.entities.GameList;
+import com.medeiros.apigamelist.projection.GameMinProjection;
 import com.medeiros.apigamelist.repositories.GameListRepository;
+import com.medeiros.apigamelist.repositories.GameRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,37 @@ public class GameListService {
   @Autowired // Injeta o repository ao service por estarem em camadas diferentes.
   private GameListRepository gameListRepository;
 
+  @Autowired
+  private GameRepository gameRepository;
+
   @Transactional(readOnly = true)
   public List<GameListDTO> findAll() {
     List<GameList> result = gameListRepository.findAll();
     List<GameListDTO> dto = result.stream().map(GameListDTO::new).toList();
 
     return dto;
+  }
+
+  @Transactional
+  public void moveGameInList(
+    Long listId,
+    int sourceIndex,
+    int destinationIndex
+  ) {
+    List<GameMinProjection> list = gameRepository.searchByList(listId);
+    GameMinProjection obj = list.remove(sourceIndex);
+
+    list.add(destinationIndex, obj);
+
+    int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+    int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+    for (int i = min; i <= max; i++) {
+      gameListRepository.updateBelongingPosition(
+        listId,
+        list.get(i).getId(),
+        i
+      );
+    }
   }
 }
