9101e0e6ee1353d417b587cc7327ca0ea16128cc
Bruno <brunodemedeiros14@gmail.com>
2023-05-28 21:47:34 -0300

    feat(credit-application-system): first commit

---
+++ Dio/TQI_Kotlin/credit-application-system/src/test/kotlin/com/medeiros/credit/application/system/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.medeiros.credit.application.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
