dac2ff0d75f4cad1a8e99c21b32c2cb7c033a095
Bruno <brunodemedeiros14@gmail.com>
2023-05-30 20:10:37 -0300

    feat(cred-app-sys): implement unit tests

--- Dio/TQI_Kotlin/credit-application-system/src/main/kotlin/com/medeiros/credit/application/system/service/impl/CustomerService.kt
+++ Dio/TQI_Kotlin/credit-application-system/src/main/kotlin/com/medeiros/credit/application/system/service/impl/CustomerService.kt
@@ -1,6 +1,7 @@
 package com.medeiros.credit.application.system.service.impl
 
 import com.medeiros.credit.application.system.entity.Customer
+import com.medeiros.credit.application.system.exception.BusinessException
 import com.medeiros.credit.application.system.repositories.CustomerRepository
 import com.medeiros.credit.application.system.service.ICustomerService
 import org.springframework.stereotype.Service
@@ -13,7 +14,7 @@ class CustomerService(
 	override fun save(customer: Customer): Customer = this.customerRepository.save(customer)
 
 	override fun findById(id: Long): Customer = this.customerRepository.findById(id).orElseThrow {
-		throw RuntimeException("Id $id not found.")
+		throw BusinessException("Id $id not found")
 	}
 
 	fun findAll(): List<Customer> = this.customerRepository.findAll()
