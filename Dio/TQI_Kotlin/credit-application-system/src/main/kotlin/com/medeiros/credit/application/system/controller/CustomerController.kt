e31d021508c9932c68dc571b28b094988d08f283
Bruno <brunodemedeiros14@gmail.com>
2023-05-29 22:49:31 -0300

    fix(cred-app-sys): use service in controller

--- Dio/TQI_Kotlin/credit-application-system/src/main/kotlin/com/medeiros/credit/application/system/controller/CustomerController.kt
+++ Dio/TQI_Kotlin/credit-application-system/src/main/kotlin/com/medeiros/credit/application/system/controller/CustomerController.kt
@@ -4,7 +4,7 @@ import com.medeiros.credit.application.system.dto.CustomerDto
 import com.medeiros.credit.application.system.dto.CustomerToUpdateDto
 import com.medeiros.credit.application.system.dto.CustomerViewDto
 import com.medeiros.credit.application.system.entity.Customer
-import com.medeiros.credit.application.system.repositories.CustomerRepository
+import com.medeiros.credit.application.system.service.impl.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -12,37 +12,37 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1/customers")
 class CustomerController(
-	private val customerRepository: CustomerRepository
+	private val customerService: CustomerService
 ) {
 
 	@GetMapping
 	fun getAllCustomer(): ResponseEntity<List<CustomerViewDto>> =
-		ResponseEntity.status(HttpStatus.OK).body(customerRepository.findAll().map {
+		ResponseEntity.status(HttpStatus.OK).body(customerService.findAll().map {
 			it.run { CustomerViewDto(firstName, lastName, cpf, income, address.zipCode, address.street) }
 		})
 
 	@PostMapping
 	fun saveCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<String> =
 		ResponseEntity.status(HttpStatus.CREATED)
-			.body("Customer ${customerRepository.save(customerDto.toEntity()).firstName} salvo com sucesso.")
+			.body("Customer ${customerService.save(customerDto.toEntity()).firstName} salvo com sucesso.")
 
 	@GetMapping("/{id}")
 	fun getById(@PathVariable id: Long): ResponseEntity<CustomerViewDto> =
-		ResponseEntity.status(HttpStatus.OK).body(customerRepository.findById(id).get().run {
+		ResponseEntity.status(HttpStatus.OK).body(customerService.findById(id).run {
 			CustomerViewDto(firstName, lastName, cpf, income, address.zipCode, address.street)
 		})
 
 	@DeleteMapping("/{id}")
-	fun deleteById(@PathVariable id: Long) = customerRepository.deleteById(id)
+	fun deleteById(@PathVariable id: Long) = customerService.delete(id)
 
 	@PatchMapping("/{id}")
 	fun updateCustomer(
 		@PathVariable id: Long,
 		@RequestBody customerToUpdateDto: CustomerToUpdateDto
 	): ResponseEntity<CustomerViewDto> {
-		val customer: Customer = customerRepository.findById(id).get()
+		val customer: Customer = customerService.findById(id)
 		val updatedCustomer: Customer = customerToUpdateDto.toEntity(customer)
-		return ResponseEntity.status(HttpStatus.OK).body(customerRepository.save(updatedCustomer).run {
+		return ResponseEntity.status(HttpStatus.OK).body(customerService.save(updatedCustomer).run {
 			CustomerViewDto(firstName, lastName, cpf, income, address.zipCode, address.street)
 		})
 	}
--- Dio/TQI_Kotlin/credit-application-system/src/main/kotlin/com/medeiros/credit/application/system/service/impl/CustomerService.kt
+++ Dio/TQI_Kotlin/credit-application-system/src/main/kotlin/com/medeiros/credit/application/system/service/impl/CustomerService.kt
@@ -9,11 +9,14 @@ import org.springframework.stereotype.Service
 class CustomerService(
 	private val customerRepository: CustomerRepository
 ) : ICustomerService {
+
 	override fun save(customer: Customer): Customer = this.customerRepository.save(customer)
 
 	override fun findById(id: Long): Customer = this.customerRepository.findById(id).orElseThrow {
 		throw RuntimeException("Id $id not found.")
 	}
+
+	fun findAll(): List<Customer> = this.customerRepository.findAll()
 
 	override fun delete(id: Long) = this.customerRepository.deleteById(id)
 
